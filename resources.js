e383cbbdb2b112d7bd0eab26735e13aebce32d5d
bailey <bailey@harperdb.io>
2026-08-02 08:34:43 -0600

    chore(deps): React 19 + plugin-react 6

--- resources.js
+++ resources.js
@@ -15022,7 +15022,7 @@ var supportHtml = `<!doctype html>
 </body>
 </html>
 `;
-var buildStamp = "0.2.0+2026-08-02T14:30:54.411Z";
+var buildStamp = "0.2.0+2026-08-02T14:32:10.484Z";
 
 // server/resources.ts
 import { randomBytes, scryptSync, timingSafeEqual } from "node:crypto";
