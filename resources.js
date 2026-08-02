67759f97db6a39fa93641b112ab8b12e30ff357f
bailey <bailey@harperdb.io>
2026-08-02 08:41:28 -0600

    chore(deps): Vite 8 / Rolldown

--- resources.js
+++ resources.js
@@ -15022,7 +15022,7 @@ var supportHtml = `<!doctype html>
 </body>
 </html>
 `;
-var buildStamp = "0.2.0+2026-08-02T14:36:34.125Z";
+var buildStamp = "0.2.0+2026-08-02T14:38:48.439Z";
 
 // server/resources.ts
 import { randomBytes, scryptSync, timingSafeEqual } from "node:crypto";
