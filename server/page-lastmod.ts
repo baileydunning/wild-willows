f23034b6957c048ca1580d42bb2b89f33c78f0b5
Bailey Dunning <bailey@harperdb.io>
2026-08-15 18:00:43 -0600

    update support page

--- server/page-lastmod.ts
+++ server/page-lastmod.ts
@@ -6,7 +6,7 @@ export const pageLastmod: Record<string, string> = {
 	"/": "2026-08-15",
 	"/privacy.html": "2026-08-12",
 	"/age-rating.html": "2026-08-15",
-	"/support.html": "2026-08-12",
+	"/support.html": "2026-08-15",
 	"/teachers": "2026-08-14",
 	"/educator-guide.pdf": "2026-08-10",
 	"/student-worksheets.pdf": "2026-08-09"
